9378c9915d55919ae69514c8499fc910316d04d3
Alex Aguiñaga <marioalejandro300402@gmail.com>
2021-10-15 20:31:36 -0500

    Solucion de bug

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-swaggerDocument = require('./swagger.json');
+const swaggerDocument = require('./swagger.json');
 const usersRoutes = require('./routes/users.routes');
 const authRoute = require('./routes/auth.routes');
 const conversationsRoutes = require('./routes/conversations.routes');
 const passRoutes = require('./routes/password.routes');
 
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', usersRoutes);
 app.use('/api/v1', authRoute);
 app.use('/api/v1', conversationsRoutes);
